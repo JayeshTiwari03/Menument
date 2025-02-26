a0a235877d4ff8e25e2f8808843671ca8bfc3cfa
Jayesh Tiwari <JayeshTiwari03@users.noreply.github.com>
2025-02-26 17:18:59 +0530

    Restructure folder

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import menuReducer from "./src/store/slices/menuSlice";
-import beverageReducer from "./src/store/slices/beveragesSlice";
-import categoryReducer from "./src/store/oldRedux/categoryReducers";
+import menuReducer from "./slices/menuSlice";
+import beverageReducer from "./slices/beveragesSlice";
+import categoryReducer from "./oldRedux/categoryReducers";
 
 export const store = configureStore({
   reducer: {
